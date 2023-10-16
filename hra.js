0a28c2806ba3bd06da72012eecc143b498a76a5f
Monika Suchanková <143554633+monasuchankova@users.noreply.github.com>
2023-10-16 13:09:59 +0200

    Add files via upload

--- hra.js
+++ hra.js
@@ -1,5 +1,7 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 const imgPlayer = document.querySelector('.game__player--symbol');
+const buttons = document.querySelectorAll('.game-panel__buton');
 
 const addSymbol = (event) => {
   if (currentPlayer === 'circle') {
@@ -15,6 +17,49 @@ const addSymbol = (event) => {
   }
 };
 
+/* --- HERNÍ POLE --- */
+
+const herniPole = Array.from(buttons).map((button) => {
+  if (button.classList.contains('game-panel__buton--circle')) {
+    return 'o';
+  } else if (button.classList.contains('game-panel__buton--cross')) {
+    return 'x';
+  }
+  return '_';
+});
+
+const vitez = findWinner(herniPole);
+if (vitez === 'o' || vitez === 'x') {
+  const winnerIs = () => {
+    alert(`Vyhrál hráč se symbolem ${vitez}.`);
+    window.location.reload();
+  };
+  setTimeout(winnerIs, 500);
+} else if (vitez === 'tie') {
+  const tieIs = () => {
+    alert(`Remíza. Musíte začít znovu`);
+    window.location.reload();
+  };
+  setTimeout(tieIs, 500);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', addSymbol);
+});
+
+/* --- RESTART --- */
+const restart = (event) => {
+  const alert = confirm('Opravdu chceš začít znovu?');
+  if (alert === false) {
+    event.preventDefault();
+  }
+};
+
+document
+  .querySelector('.game__buttons--restart')
+  .addEventListener('click', restart);
+
+/* --- odložené ---
 document
   .querySelector('button:nth-child(1)')
   .addEventListener('click', addSymbol);
@@ -53,16 +98,4 @@ document
 
 document
   .querySelector('button:nth-child(10)')
-  .addEventListener('click', addSymbol);
-
-/* --- GAME BONUS --- */
-const restart = (event) => {
-  const alert = confirm('Opravdu chceš začít znovu?');
-  if (alert === false) {
-    event.preventDefault();
-  }
-};
-
-document
-  .querySelector('.game__buttons--restart')
-  .addEventListener('click', restart);
+  .addEventListener('click', addSymbol);*/
